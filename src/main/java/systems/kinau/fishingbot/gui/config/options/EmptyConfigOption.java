3db84c628e84108e4547fe2f526d3b0c3b7fe364
MrMelon <github@onpointcoding.net>
2022-07-09 14:01:10 +0100

    Remove unused EmptyConfigOption constructor

--- src/main/java/systems/kinau/fishingbot/gui/config/options/EmptyConfigOption.java
+++ src/main/java/systems/kinau/fishingbot/gui/config/options/EmptyConfigOption.java
@@ -4,10 +4,6 @@ package systems.kinau.fishingbot.gui.config.options;
 import javafx.scene.control.Label;
 
 public class EmptyConfigOption extends ConfigOption {
-    public EmptyConfigOption(String key, String description, Object value) {
-        super(key, description, null);
-    }
-
     public EmptyConfigOption(String key, String description) {
         super(key, description, null);
         Label emptyLabel = new Label(description);
