5fb3b4e45ff93c3ba1087960049c45e123503899
David Luedtke <david@ldtke.de>
2025-11-30 04:43:27 +0100

    1.21.11-pre3

--- src/main/java/systems/kinau/fishingbot/bot/registry/Registries.java
+++ src/main/java/systems/kinau/fishingbot/bot/registry/Registries.java
@@ -57,6 +57,7 @@ public class Registries {
         BUNDLED_REGISTRIES.add(ProtocolConstants.MC_1_21_6);
         BUNDLED_REGISTRIES.add(ProtocolConstants.MC_1_21_7);
         BUNDLED_REGISTRIES.add(ProtocolConstants.MC_1_21_9);
+        BUNDLED_REGISTRIES.add(ProtocolConstants.MC_1_21_11);
 
         ITEM = new ItemRegistry();
         ENTITY_TYPE = new EntityTypeRegistry();
